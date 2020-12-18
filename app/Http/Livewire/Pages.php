9ee46df0ef33322d81dc58b918ac546f65ff8fa5
Syahin Syah <syahinsyah7@gmail.com>
2020-12-18 17:56:08 +0800

    title with slug parallel save into db

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -3,16 +3,62 @@
 namespace App\Http\Livewire;
 
 use App\Models\Page;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class Pages extends Component
 {
-    public $modalFormVisible = false ; //ni kena check balik apa dia cakap . tapi kena buat 
+    public $modalFormVisible =true; //ni kena check balik apa dia cakap . tapi kena buat 
     public $slug;
-    public $titile;
-    public $content;  
+    public $title;
+    public $context;  
+
     
     /**
+     * the validation rules
+     * 
+     *
+     * @return void
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'slug' => ['required',Rule::unique('pages', 'slug')],
+            'context' => 'required',
+        ];
+    }
+    
+    /**
+     * updatedTitle everytime he update it
+     * turn into slug
+     *
+     * @param  mixed $value
+     * @return void
+     */
+    public function updatedTitle($value)
+    {
+
+        $this->generateSlug($value);
+
+    }
+        
+    /**
+     * This for create into the database pages
+     *
+     * @return void
+     */
+    public function create()
+    {
+        $this->validate();
+        Page::create($this->modelData()); 
+        $this->modalFormVisible =false; //hide model punya 
+        $this->cleanVars();
+    }
+
+    
+
+    /** control shift i
      * show the form modal 
      * of the create function.
      *
@@ -23,6 +69,48 @@ class Pages extends Component
         $this->modalFormVisible = true; 
     }
     
+    //seperate the asignemnet of data in another fx for cleaner look    
+    /**
+     * The data for the model mapped 
+     * in this component.
+     *
+     * @return void
+     */
+    public function modelData()
+    {
+        return [
+            'title'=> $this->title,
+            'slug' => $this->slug,
+            'context' => $this->context
+        ];
+    }
+
+    
+    /**
+     * Reset var to null balik
+     *
+     * @return void
+     */
+    public function cleanVars()
+    {
+        $this->title =null;
+        $this->slug=null;
+        $this->context= null;
+    }
+    
+    /**
+     * generateSlug a url sliug based on the title
+     *
+     * @param  mixed $value
+     * @return void
+     */
+    private function generateSlug($value)
+    {
+        $process1 = str_replace('','-',$value);
+        $process2 = strtolower($process1);
+        $this->slug = process2;
+    }
+
     /**
      * the livewire render function.
      *
