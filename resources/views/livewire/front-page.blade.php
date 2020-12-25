bdcc87cc47a5d499c10301f06b8ec81cc937e577
Syahin Syah <syahinsyah7@gmail.com>
2020-12-25 22:23:52 +0800

    implement tailwind , test image and flexible

--- resources/views/livewire/front-page.blade.php
+++ resources/views/livewire/front-page.blade.php
@@ -1,4 +1,17 @@
 <div>
-   <h1> This is {{$title}} </h1> 
-   {!! $context !!}
+   <div class="border p-4 text-gray-100 bg-gray-400
+    text-3xl sm:bg-blue-500 md:bg-red-600">{{$title}} </div>
+   <div>
+      <div class="border p-4 text-center">
+         {!! $context !!}
+         saya adalah syahin
+      </div>
+      <div>
+        
+      </div>
+</div>  
+ <div class="border"> 
+    <img class="w-full h-full object-cover object-center" src="{{'img/profilepicture.jpg'}}"
+</div>
+   
 </div>
